69217260035efa055018157e77284155ad2ef631
chenk <chenk@jsyl.com.cn>
2020-07-29 10:09:34 +0800

    修改js引入

--- files/src/main/java/com/ck/files/FilesApplication.java
+++ files/src/main/java/com/ck/files/FilesApplication.java
@@ -2,8 +2,10 @@ package com.ck.files;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = "com.ck.*")
 public class FilesApplication {
 
     public static void main(String[] args) {
---
+++ files/src/main/java/com/ck/files/UsingStaticController.java
@@ -0,0 +1,19 @@
+package com.ck.files;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @Author chenk
+ * @create 2020/7/29 10:03
+ */
+@Configuration
+public class UsingStaticController extends WebMvcConfigurationSupport {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
+
